fff7775335888001e3675101c4154ae038ac9f7a
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-06-26 12:59:40 +0300

    Add project files.

---
+++ Entities/concrete/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.concrete
+{
+    internal class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public DateTime ModelYear { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
